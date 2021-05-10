9e79f256398adf6abf1e79711224fc30dc923311
taiwo2 <adetonat@ymail.com>
2021-05-10 17:55:05 +0100

    add

--- test/general.test.js
+++ test/general.test.js
@@ -1,4 +1,4 @@
-import General from '../src/helper/general';
+import General from '../src/helpers/general';
 
 test('When pass a number with less than 6 digits and a string size of 6, it returns the same number with zeros in the left.', () => {
   const zeroPad = General.zeroPad(1523, 6);
